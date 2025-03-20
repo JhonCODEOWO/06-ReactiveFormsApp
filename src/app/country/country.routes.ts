28b6ab2da80a38d2fa6c154cd65d5bd7e766bac6
Jonathan Juárez Valera <jjv20618@gmail.com>
2025-03-20 09:44:32 -0600

    Inicio de la aplicación

---
+++ src/app/country/country.routes.ts
@@ -0,0 +1,8 @@
+import {Routes} from '@angular/router'
+import { CountryPageComponent } from './pages/country-page/country-page.component'
+export const authRoutes: Routes = [
+    {
+        path: '',
+        component: CountryPageComponent,
+    }
+]
