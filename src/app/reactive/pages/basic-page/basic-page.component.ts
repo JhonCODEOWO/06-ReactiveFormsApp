a6baf607151430a18616c94e92f31eb15531b97b
Jonathan Juárez Valera <jjv20618@gmail.com>
2025-03-20 11:58:58 -0600

    Reutilizar lógica de validación de formularios

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,6 +1,7 @@
 import { JsonPipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-basic-page',
@@ -10,6 +11,7 @@ import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } 
 })
 export class BasicPageComponent {
   private fb = inject(FormBuilder);
+  formUtils = FormUtils;
 
   myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
@@ -23,31 +25,9 @@ export class BasicPageComponent {
   //   inStorage: new FormControl(0),
   // })
 
-  isValidField(fieldName: string): boolean | null {
-    return (this.myForm.controls[fieldName].errors && this.myForm.controls[fieldName].touched)
-  }
-
-  getFieldError(fieldName: string): string | null {
-    if (!this.myForm.controls[fieldName]) return null;
-
-    const errors = this.myForm.controls[fieldName].errors ?? {};
-
-    for(const key of Object.keys(errors)){
-      switch(key){
-        case 'required':
-          return 'Este campo es requerido';
-        
-        case 'minlength':
-          return `Mínimo de ${errors['minlength'].requiredLength} caracteres.`
-        
-        case 'min':
-          return `Valor mínimo ${errors['min'].min}`
-        
-      }
-    }
-
-    return null;
-  }
+  // isValidField(fieldName: string): boolean | null {
+  //   return (this.myForm.controls[fieldName].errors && this.myForm.controls[fieldName].touched)
+  // }
 
   onSave(){
     if(this.myForm.invalid) {
---
+++ src/app/utils/form-utils.ts
@@ -0,0 +1,29 @@
+import { FormGroup } from '@angular/forms';
+
+export class FormUtils {
+  static isValidField(fieldName: string, form: FormGroup): boolean | null {
+    return form.controls[fieldName].errors && form.controls[fieldName].touched;
+  }
+
+  static getFieldError(fieldName: string, form: FormGroup): string | null {
+    if (!form.controls[fieldName]) return null;
+
+    const errors = form.controls[fieldName].errors ?? {};
+
+    for(const key of Object.keys(errors)){
+      switch(key){
+        case 'required':
+          return 'Este campo es requerido';
+        
+        case 'minlength':
+          return `Mínimo de ${errors['minlength'].requiredLength} caracteres.`
+        
+        case 'min':
+          return `Valor mínimo ${errors['min'].min}`
+        
+      }
+    }
+
+    return null;
+  }
+}
