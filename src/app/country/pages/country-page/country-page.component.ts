48c15d276d767061142f87d10bb52d1e2769f948
Jonathan Juárez Valera <jjv20618@gmail.com>
2025-03-21 16:17:01 -0600

    Suscribirse a cambios del formulario y llenar segundo select

--- src/app/country/pages/country-page/country-page.component.ts
+++ src/app/country/pages/country-page/country-page.component.ts
@@ -1,8 +1,9 @@
 import { JsonPipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, inject, signal } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CountryService } from '../../services/country.service';
 import { Country } from '../../interfaces/country.interface';
+import { catchError, of, Subscription, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
@@ -23,4 +24,43 @@ export class CountryPageComponent {
     country: ['', Validators.required],
     border: ['', Validators.required],
   })
+
+  //Efecto que se va a realizar siempre que un valor cambie
+  onFormChange = effect((onCleanup) => {
+    const formSubscription = this.onRegionChanged();
+
+    onCleanup(() => {
+      formSubscription.unsubscribe();
+      console.log('Limpiado');
+    })
+  })
+
+  //Método que devuelve la suscripción
+  onRegionChanged(): Subscription{
+    return this.myForm.get('region')!.valueChanges
+    //Pipes para utilizar los datos antes de resolverse
+    .pipe(
+      //Efectos que limpian el select a ninguno antes de que entre la suscripción
+      tap(() => this.myForm.get('country')?.setValue('')),
+      tap(() => this.myForm.get('border')?.setValue('')),
+      //Efectos en un solo tap que van a limpiar los datos de countries borders 
+      tap(() => {
+        this.countriesBorders.set([]);
+        this.countriesByRegion.set([])
+      }),
+      //Cambia el observable original por el que se utilice aquí alterando el flujo y fuente de datos
+      switchMap(region => this.countryService.getCountriesByRegion(region ?? '').pipe(catchError(()=> of([]))))
+    )
+    .subscribe({
+      next: countries => {
+        this.countriesByRegion.set(countries);
+      },
+      error: (error) => {
+        console.log('Ha ocurrido un error');
+      }
+    })
+  }
+  // formRegionChanged = this.myForm.get('region')?.valueChanges.subscribe(value => {
+  //   console.log(value);
+  // })
 }
