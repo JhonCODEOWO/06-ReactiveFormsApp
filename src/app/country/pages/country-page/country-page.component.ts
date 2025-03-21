a50e926a1ad0e4a9e3f1de2ae94381236b6dd73d
Jonathan Juárez Valera <jjv20618@gmail.com>
2025-03-21 15:00:33 -0600

    Primer selector controlado: Continentes

--- src/app/country/pages/country-page/country-page.component.ts
+++ src/app/country/pages/country-page/country-page.component.ts
@@ -1,9 +1,26 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CountryService } from '../../services/country.service';
+import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'app-country-page',
-  imports: [],
+  imports: [JsonPipe,ReactiveFormsModule],
   templateUrl: './country-page.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CountryPageComponent { }
+export class CountryPageComponent {
+  fb = inject(FormBuilder);
+  countryService = inject(CountryService);
+
+  regions = signal(this.countryService.regions);
+
+  countriesByRegion = signal<Country[]>([]);
+  countriesBorders = signal<Country[]>([]);
+
+  myForm = this.fb.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    border: ['', Validators.required],
+  })
+}
